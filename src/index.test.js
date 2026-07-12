dab8821c81235a684891f05f5aa70eca548af0be
Izzy343 <isaiahmfentress@gmail.com>
2026-07-12 00:16:17 -0700

    Added by function and test

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,10 @@
 const sayHi = require('./index.js')
+const sayBye = require('./index.js')
 
 test("Says 'Hello Mike' when 'Mike' is passed", () => {
    expect(sayHi('Mike')).toBe('Hello Mike')
+})
+
+test("Says 'Bye Mike' when 'Mike' is passed", () => {
+   expect(sayBye('Mike')).toBe('Bye Mike')
 })
