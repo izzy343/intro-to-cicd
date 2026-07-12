e3f0ba934524240e91ffd380e0d26c4ca18191c3
Izzy343 <isaiahmfentress@gmail.com>
2026-07-12 00:22:12 -0700

    trying again

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,9 @@
 const sayHi = require('./index.js')
-const sayBye = require('./index.js')
-
 test("Says 'Hello Mike' when 'Mike' is passed", () => {
    expect(sayHi('Mike')).toBe('Hello Mike')
 })
 
+const sayBye = require('./index.js')
 test("Says 'Bye Mike' when 'Mike' is passed", () => {
    expect(sayBye('Mike')).toBe('Bye Mike')
 })
